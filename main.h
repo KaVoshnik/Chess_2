9fe3dbc558911665c2e94ee627a08441951f007d
KaVoshnik <hikkan008@gmail.com>
2024-09-02 17:29:58 +0000

    modified:   main.h

--- main.h
+++ main.h
@@ -1,4 +1,41 @@
+#ifndef class_h
+#define class_h
 #include <iostream>
+#include <string>
 
 using std::cout;
 
+class Piece {
+
+public:
+    Piece(Color color, PieceType type, int row, int col): //Конструктор  
+    color(color), type(type), row(row), col(col) {}
+    
+    getColor() const {
+        return color;
+    } //Цвет фигуры
+    getType() const {
+        return type;
+    }  //Тип фигуры
+    
+    int getRow() const {
+        return row;
+    } 
+    int getCol() const {
+        return col;
+    } 
+    void setRow(int newRow)const {
+        row = newRow;
+    } 
+    void setCol(int newCol) const {
+        col = newCol;
+    } 
+
+private:
+    Color color;
+    PieceType type;
+    int row;
+    int col;
+}
+
+#endif
