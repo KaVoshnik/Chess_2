478aad284527c3816b6e99e50b66d117c7cadf9d
Avairon <lipsknaz@gmail.com>
2024-09-06 15:57:15 +0700

    modified:   include/Board.h 	modified:   src/Board.cpp 	deleted:    test_to_fix.cpp 	modified:   testing.cpp

--- testing.cpp
+++ testing.cpp
@@ -4,5 +4,8 @@ int main() {
     Board board = board.set_base8x8(); //init basic 8x8 board with pieces
     
     board.printBoard(); //output all board statement
+    board.moveFromTo(0, 0, 2, 0);
+    board.printBoard();
+
     return 0;
 }
