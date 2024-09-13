f5ca79cab3537a8db7a608e9df1a79490aee357f
Avairon <lipsknaz@gmail.com>
2024-09-13 14:14:10 +0700

    modified:   src/Board.cpp

--- src/Board.cpp
+++ src/Board.cpp
@@ -159,3 +159,5 @@ void Board::printBoard(){
 void Board::moveFromTo(int x1, int y1, int x2, int y2){
     board[x2][y2] = board[x1][y1];
     board[x2][y2].newCords(x2, y2);
+    board[x1][y1].changePiece(WHITE, NAIN, x1, y1);
+}
