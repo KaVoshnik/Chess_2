8ec7bdb8b63f8fc992f43bf40001a5b8eb230243
Avairon <lipsknaz@gmail.com>
2024-09-15 17:03:32 +0700

    modified:   test.cpp

--- test.cpp
+++ test.cpp
@@ -1,5 +1,11 @@
-#include "Board.h"
+#include "./src/Board.h"
 
 int main(){
-    
+    Board board = board.set_base8x8();
+    board.printBoard();
+
+    board.resizeBoard(10);
+    board.printBoard();
+
+    return 0;
 }
