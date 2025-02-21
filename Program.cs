b2fe617dba511f3e07e8985acc3dde2a91e73239
kavoshnik <hikkan008@gmail.com>
2025-02-21 13:46:10 +0700

    pulling change from avairon-dev

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿//using System;
+//using Piece;
+using Board;
+
+namespace test_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            board board = new board(); // board
+            
+            board.create_piece("pawn", 2, 1);
+
+            board.print_field_info(-2, -2, 8, 8);    
+
+            board.print_field_info(0, 0, 4, 6);   
+        }  
+    }
+}
+
+
+
