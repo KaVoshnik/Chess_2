8d5c3ba3a9bc8eb9f150ded59cf4dec6f930c662
kavoshnik <hikkan008@gmail.com>
2025-02-26 16:39:25 +0700

    modified:   lib/Board.cs

--- lib/Board.cs
+++ lib/Board.cs
@@ -5,8 +5,7 @@ namespace Board
     public class board
     {
         private int[] start_pos = new int[2];
-        private piece[] content = new piece[0];
-
+        private List<piece> content = new List<piece>();
 
         public board(){
             start_pos[0] = 0;
@@ -22,31 +21,25 @@ namespace Board
             return start_pos;
         }
 
-        public piece[] get_content(){
+        public List<piece> get_content(){
             return content;
         }
-
+        public void set_content(List<piece> Content){
+            content = Content;
+        }
         public void set_start_pos(int[] StartPos){
             start_pos[0] = StartPos[0];
             start_pos[1] = StartPos[1];
         }
 
-        public void set_content(piece[] Content){
-            content = Content;
+        public void add_content(piece Content)
+        {
+            content.Add(Content);
         }
 
-        public void add_content(piece Content){
-            Array.Resize(ref content, content.Length + 1);
-            content[content.Length - 1] = Content;
-        }
-
-        public piece? get_piece_by_cords(int x, int y){
-            for(int i = 0; i < content.Length; i++){
-                if(content[i].get_xpos() == x && content[i].get_ypos() == y){
-                    return content[i];
-                }
-            }
-            return null;
+        public piece? get_piece_by_cords(int x, int y)
+        {
+             return content.Find(p => p.get_xpos() == x && p.get_ypos() == y);
         }
 
         private int? search_id_by_cords(int x, int y){
@@ -57,31 +50,19 @@ namespace Board
             }
             return null;
         }
-        public void remove_by_cords(int x, int y){ // AAAAAAAAAAAAAAAAAAA FIX IT
-            
+        public bool remove_by_cords(int x, int y)
+        {
+            int index = content.FindIndex(p => p.get_xpos() == x && p.get_ypos() == y);
 
-            piece[] buff = new piece[0];
-            int buff_id = 0;
-            int searched_number = 0;
-
-            if(search_id_by_cords(x, y) == null){
-                return;
+            if (index == -1)
+            {
+                return false;
             }
 
-            searched_number = search_id_by_cords(x, y).Value;
-
-            for(int i = 0; i < content.Length; i++){
-                if(i == searched_number){
-                    continue;
-                }
-                Array.Resize(ref buff, buff.Length + 1);
-                buff[buff_id] = content[i];
-                buff_id++;
-            }
-
-            Array.Resize(ref content, content.Length - 1);
-            content = buff;
+            content.RemoveAt(index);
+            return true;
         }
+
         public void print_field(int start_x, int start_y, int size_x, int size_y){
             /*
             y
